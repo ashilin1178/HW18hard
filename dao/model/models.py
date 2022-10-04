c04a106a86826894139c256c9c57e10eef3587c3
Anton <ashilin1178@gmail.com>
2022-10-04 21:25:24 +0800

    models and schema

---
+++ dao/model/models.py
@@ -0,0 +1,33 @@
+# здесь модель SQLAlchemy для сущности, также могут быть дополнительные методы работы с моделью (но не с базой,
+# с базой мы работаем в классе DAO)
+
+
+from setup_db import db
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    movies = db.relationship("Movie")
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    movies = db.relationship("Movie")
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
